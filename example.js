f584babc226b6d6e69838b908a6447a1ccc2d464
Clayton Yelland <cyelland@williamsburg.k12.ia.us>
2022-10-19 18:08:46 -0500

    some js example code

---
+++ example.js
@@ -0,0 +1,13 @@
+var stringVariable = "clayton is learing to code";
+
+var numberVariable = 6;
+
+console.log(stringVariable, typeof stringVariable);
+console.log(numberVariable, typeof numberVariable);
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+console.log('function output =>', add(2, 2));
+console.log(add)
